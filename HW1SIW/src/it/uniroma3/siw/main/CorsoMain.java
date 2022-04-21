c164742a96f62b732caf82eab32e4f273f53c03b
Ale300798 <104078601+Ale300798@users.noreply.github.com>
2022-04-21 09:35:12 +0200

    Add files via upload

---
+++ HW1SIW/src/it/uniroma3/siw/main/CorsoMain.java
@@ -0,0 +1,21 @@
+package it.uniroma3.siw.main;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+
+public class CorsoMain {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("corso-unit");
+		EntityManager em = emf.createEntityManager();
+
+		em.close();
+		emf.close();
+	}
+
+
+}
